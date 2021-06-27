6ab78700b3a8dc4fb022f47dd1264f6561903748
ayushdev351 <79743704+ayushdev351@users.noreply.github.com>
2021-06-27 11:14:00 +0530

    Add Button Styling

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,11 @@ function Header(props) {
     <HeaderContainer>
       <header className="header">
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderButton>
+          <span className="icon"><i class="fas fa-shopping-cart"></i></span>
+          <span>Your Cart</span>
+          <span className="badge">3</span>
+        </HeaderButton>
       </header>
       <div>
         <img src={mealsImage} alt="food" />
@@ -48,4 +52,67 @@ const HeaderContainer = styled.div`
     object-fit: cover;
     transform: rotateZ(-5deg) translateY(-4rem) translateX(-1rem);
   }
+`;
+
+
+const HeaderButton = styled.button`
+  
+  cursor: pointer;
+  font: inherit;
+  border: none;
+  background-color: #4d1601;
+  color: white;
+  padding: 0.75rem 3rem;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border-radius: 25px;
+  font-weight: bold;
+
+
+:hover,
+:active {
+  background-color: #2c0d00;
+}
+
+.icon {
+  width: 1.35rem;
+  height: 1.35rem;
+  margin-right: 0.5rem;
+}
+
+.badge {
+  background-color: #b94517;
+  padding: 0.25rem 1rem;
+  border-radius: 25px;
+  margin-left: 1rem;
+  font-weight: bold;
+}
+
+:hover .badge,
+:active .badge {
+  background-color: #92320c;
+}
+
+.bump {
+  animation: bump 300ms ease-out;
+}
+
+@keyframes bump {
+  0% {
+    transform: scale(1);
+  }
+  10% {
+    transform: scale(0.9);
+  }
+  30% {
+    transform: scale(1.1);
+  }
+  50% {
+    transform: scale(1.15);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
 `;
